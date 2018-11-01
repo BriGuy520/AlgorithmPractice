b39f71f9f35dafbd62888716f07bc9affb8e7975
Brian Falasz <brianfalasz@gmail.com>
2018-11-01 11:56:31 -0500

    adding another algorithm problem from freeCodeCamp.org

---
+++ freeCodeCamp/seekAndDestroy.js
@@ -0,0 +1,24 @@
+//  Intermediate Algorithm Scripting: Seek and Destroy
+//  You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+//
+// Note
+// You have to use the arguments object.
+//
+// Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+// This answer is completely my own...
+
+function destroyer(arr, ...args) {
+  let newArr = [];
+  arr.forEach(function(item){
+    if(args.includes(item) === false){
+      newArr.push(item);
+    }
+  });
+
+  return newArr;
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+// This solution passed all tests at freeCodeCamp.org 
