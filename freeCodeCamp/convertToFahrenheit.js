408ada919d739d304011520dc852173f90a6225b
Brian Falasz <brianfalasz@gmail.com>
2018-11-14 13:16:47 -0600

    adding the convert to fahrenheit problem from freeCodeCamp.org

---
+++ freeCodeCamp/convertToFahrenheit.js
@@ -0,0 +1,19 @@
+//Basic Algorithm Scripting: Convert Celsius to Fahrenheit
+//
+//The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
+//
+//You are given a variable celsius representing a temperature in Celsius. Use the variable fahrenheit already defined and assign it the Fahrenheit temperature equivalent to the given Celsius temperature. Use the algorithm mentioned above to help convert the Celsius temperature to Fahrenheit.
+//
+//Don't worry too much about the function and return statements as they will be covered in future challenges. For now, only use operators that you have already learned.
+
+
+function convertToF(celsius) {
+  let fahrenheit = (9/5 * celsius) + 32;
+  return fahrenheit;
+}
+
+convertToF(30);
+
+// I got this question from the basic algorithm section on freeCodeCamp.org
+
+// This answer is completely my own.
