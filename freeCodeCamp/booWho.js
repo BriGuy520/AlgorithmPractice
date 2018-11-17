02704337bd47f03174247ddb264ba2375a954711
Brian Falasz <brianfalasz@gmail.com>
2018-11-17 15:09:06 -0600

    adding my answer the booWho algorithm problem from freeCodeCamp.org

---
+++ freeCodeCamp/booWho.js
@@ -0,0 +1,16 @@
+// Basic Algorithm Scripting: Boo who
+// Check if a value is classified as a boolean primitive. Return true or false.
+//
+// Boolean primitives are true and false.
+//
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your
+// own code.
+
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  return (typeof bool === 'boolean' ? true : false );
+}
+
+booWho(null);
+
+// This answer is completely my own.
