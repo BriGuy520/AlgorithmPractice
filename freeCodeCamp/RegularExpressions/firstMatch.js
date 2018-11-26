63024db8e4a8e028a76b082b932f038a5ffca9cc
Brian Falasz <brianfalasz@gmail.com>
2018-11-26 12:57:07 -0600

    adding more regular expression problems from freeCodeCamp.org.

---
+++ freeCodeCamp/RegularExpressions/firstMatch.js
@@ -0,0 +1,15 @@
+// Regular Expressions: Find More Than the First Match
+
+// Using the regex starRegex, find and extract both "Twinkle" words from the
+// string twinkleStar.
+
+// Note: You can have multiple flags on your regex like /search/gi
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
+
+
+// This answer is completely my own
+
+// This answer passed all tests at freeCodeCamp.org
---
+++ freeCodeCamp/RegularExpressions/wildCards.js
@@ -0,0 +1,13 @@
+// Regular Expressions: Match Anything with Wildcard Period
+
+// Complete the regex unRegex so that it matches the strings 
+// "run", "sun", "fun", "pun", "nun", and "bun". Your regex 
+// should use the wildcard character.
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let result = unRegex.test(exampleStr);
+
+// This answer is completely my own.
+
+// This passed all tests at freeCodeCamp.org
