e146399f52d10eaca0127b3a4445dc3e64e2df1f
Brian Falasz <brianfalasz@gmail.com>
2018-11-30 15:41:34 -0600

    adding my solution to the all or nothing problem from the regular expressions section at freeCodeCamp.org

---
+++ freeCodeCamp/RegularExpressions/allOrNone.js
@@ -0,0 +1,12 @@
+// Regular Expressions: Check for All or None
+
+// Change the regex favRegex to match both the American English (favorite) and
+// the British English (favourite) version of the word.
+
+let favWord = "favorite";
+let favRegex = /favou?rite/; // Change this line
+let result = favRegex.test(favWord);
+
+// This answer is completely my own
+
+// This answer passed all tests at freeCodeCamp.org
