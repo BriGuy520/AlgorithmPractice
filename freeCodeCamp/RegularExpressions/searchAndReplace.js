eaa7a7f467235ce7c3233e39b6a8c1c301d4c918
Brian Falasz <brianfalasz@gmail.com>
2018-11-30 19:20:09 -0600

    adding my solution to the search and replace problem in the regExp section at freeCodeCamp.org

---
+++ freeCodeCamp/RegularExpressions/searchAndReplace.js
@@ -0,0 +1,13 @@
+// Regular Expressions: Use Capture Groups to Search and Replace
+
+// Write a regex so that it will search for the string "good". Then update the
+// replaceText variable to replace "good" with "okey-dokey".
+
+let huhText = "This sandwich is good.";
+let fixRegex = /good/; // Change this line
+let replaceText = "okey-dokey"; // Change this line
+let result = huhText.replace(fixRegex, replaceText);
+
+// This answer is completely my own.
+
+// This answer passed all tests at freeCodeCamp.org
