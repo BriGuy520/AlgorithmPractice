e02bbeff21af62b7e7e1ebb12cc531525c92e9ca
Brian Falasz <brianfalasz@gmail.com>
2019-03-13 01:28:58 -0500

    my solution to find vowels problem

---
+++ vowels.js
@@ -0,0 +1,29 @@
+// Directions 
+
+// Write a function that returns the number of vowels
+// used in a string. Vowels are the characters 'a', 'e'
+// 'i', 'o', 'u'
+
+// Examples 
+
+//  vowels('Hi There!') -> 3
+// vowels('Why do you ask?') -> 4
+// vowels('Why?') -> 0
+
+function vowels(str){
+    const vowels = 'aeiou'.split('');
+    let count = 0;
+    
+    str.toLowerCase().split('').forEach(char => {
+        vowels.forEach(vowel => {
+            if(char === vowel){
+                count++;
+            }
+        });
+    });
+
+    return count;
+
+}
+
+vowels('Hi There!');
