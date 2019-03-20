b6ac28fc0e7d9c7aea275852d9802fcae54bd147
Brian Falasz <brianfalasz@gmail.com>
2019-03-20 01:17:08 -0500

    my solution to the weave problem

---
+++ weave/queue.js
@@ -0,0 +1,17 @@
+class Queue {
+    constructor(){
+        this.data = [];
+    }
+
+    add(record){
+        this.data.unshift(record);
+    }
+
+    remove(){
+        return this.data.pop();
+    }
+
+    peek(){
+        return this.data[this.data.length - 1];
+    }
+}
