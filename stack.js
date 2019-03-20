4f438a2b55df8c26f1b88126f10e002cbfb55903
Brian Falasz <brianfalasz@gmail.com>
2019-03-20 01:54:48 -0500

    fixed some syntax errors

--- stack.js
+++ stack.js
@@ -1,18 +1,18 @@
 // setting object methods using the prototype
 
-const Stack = () => {
+const Stack = function(){
     this.data = [];
 }
 
-Stack.prototype.push = (item) => {
+Stack.prototype.push = function(item){
     this.data.push();
 }
 
-Stack.prototype.pop = () => {
+Stack.prototype.pop = function(){
     return this.data.pop();
 }
 
-Stack.prototype.peek = () => {
+Stack.prototype.peek = function() {
     return this.data[this.data.length - 1];
 }
 
@@ -30,29 +30,29 @@ s1.peek()
 
 // using class to set methods
 
-class Stacking {
-    constructor(){
-        this.data = [];
-    }
+// class Stacking {
+//     constructor(){
+//         this.data = [];
+//     }
 
-    push(item){
-        this.data.push(item);
-    }
+//     push(item){
+//         this.data.push(item);
+//     }
 
-    pop(){
-        return this.data.pop();
-    }
+//     pop(){
+//         return this.data.pop();
+//     }
 
-    peek(){
-        return this.data[this.data.length - 1];
-    }
-}
+//     peek(){
+//         return this.data[this.data.length - 1];
+//     }
+// }
 
-const s2 = new Stacking();
-s2.push(8);
-s2.push(11);
-s2.push('I love you');
-s2;
-s2.pop();
-s2;
-s2.peek();
+// const s2 = new Stacking();
+// s2.push(8);
+// s2.push(11);
+// s2.push('I love you');
+// s2;
+// s2.pop();
+// s2;
+// s2.peek();
