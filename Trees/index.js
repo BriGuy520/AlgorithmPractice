e05b9a3fe6e2b6dc5a4eacf90cbeb51beda3c912
Brian Falasz <brianfalasz@gmail.com>
2019-04-07 14:44:18 -0500

    implemented a data tree

---
+++ Trees/index.js
@@ -0,0 +1,40 @@
+// Directions
+
+// 1) Create a node class. The constructor 
+// should accept an argument that gets assigned
+// to the data property and initialize an 
+// empty array for storing children. The node 
+// class should have methods 'add' and 'remove'.
+// 2) Create a tree class. The tree constructor
+// should initialize a 'root' property to null.
+// 3) Implement 'traverseBF' and 'traverseDF'
+// on the tree class.
+
+class Node {
+  constructor(data){
+    this.data = data;
+    this.children = [];
+  }
+
+  add(data){
+    let node = new Node(data);
+    this.children.push(node);
+  }
+
+  remove(data){
+      this.children = this.children.filter(node => {
+      return node.data !== data;
+    });
+  }
+}
+
+class Tree {
+  constructor(){
+    this.root = null;
+  }
+}
+
+let node = new Node("Hello Tree");
+let tree = new Tree();
+tree.root = node;
+
