1d805629e97d72c249c2bc66aadb009fcdf05344
Brian Falasz <brianfalasz@gmail.com>
2019-11-05 22:37:43 -0600

    adding my solution to code wars kata

---
+++ codeWars/dataReverse.js
@@ -0,0 +1,34 @@
+// A stream of data is received and needs to be reversed.
+
+// Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+
+// 11111111  00000000  00001111  10101010
+//  (byte1)   (byte2)   (byte3)   (byte4)
+// should become:
+
+// 10101010  00001111  00000000  11111111
+//  (byte4)   (byte3)   (byte2)   (byte1)
+
+
+function dataReverse(data){
+  
+  let newArr = [];
+  let finalArr = [];
+  
+  for(let i = 0; i < data.length; i++){
+    
+    if(newArr.length < 8){
+      newArr.push(data[i]);
+    } else {
+   		finalArr.push(newArr.join(""));
+      newArr = [];
+    }
+  }
+  
+  return finalArr.join(" ");
+  }
+
+const data1 = [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0];
+const data2 = [1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1];
+
+dataReverse(data1);
