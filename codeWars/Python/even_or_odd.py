283ccbbfd3523c23ed726159c19b3824f06077a7
Brian Falaz <brianfalasz@gmail.com>
2021-12-11 09:44:57 -0600

    adding my solution to even or odd solution in python

---
+++ codeWars/Python/even_or_odd.py
@@ -0,0 +1,19 @@
+# Create a function that takes an integer as an argument and returns "Even" for even 
+# numbers or "Odd" for odd numbers.
+
+def even_or_odd(number):
+
+    if number % 2 == 0:
+        print("Even")
+        return "Even"
+    else:
+        print("Odd")
+        return "Odd"
+
+
+even_or_odd(56)
+even_or_odd(38)
+even_or_odd(43)
+even_or_odd(33)
+even_or_odd(8632)
+even_or_odd(8648723)
