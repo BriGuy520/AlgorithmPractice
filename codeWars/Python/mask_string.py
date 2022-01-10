37466ef6431591893be38acb6dd8623ca117c9a5
Brian Falasz <bfalasz@yahoo.com>
2022-01-09 22:00:20 -0600

    adding my solution to masking problem in python

---
+++ codeWars/Python/mask_string.py
@@ -0,0 +1,33 @@
+# Usually when you buy something, you're asked whether your credit card number, phone number or answer
+# to your most secret question is still correct. However, since someone could look over your shoulder, 
+# you don't want that shown on your screen. Instead, we mask it.
+
+# Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+# Examples
+# maskify("4556364607935616") == "############5616"
+# maskify(     "64607935616") ==      "#######5616"
+# maskify(               "1") ==                "1"
+# maskify(                "") ==                 ""
+
+# # "What was the name of your first pet?"
+# maskify("Skippy")                                   == "##ippy"
+# maskify("Nananananananananananananananana Batman!") == "#################
+
+
+# return masked string
+def maskify(cc):
+  
+  new_str = cc
+    
+  if len(cc) <= 4:
+    return cc
+  else:
+    for idx in range(0, len(cc) - 4):
+        cc = cc[:idx] + "#" + cc[idx + 1:]
+      
+    print(cc)
+
+    return new_str
+            
+maskify("4556364607935616")
