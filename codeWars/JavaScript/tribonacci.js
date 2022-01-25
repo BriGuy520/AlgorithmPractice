d6f4f09fae4e452d989a5c1ac2bcc9cf4d8c19bd
Brian Falasz <bfalasz@yahoo.com>
2022-01-24 20:14:44 -0600

    adding my working solution to the tribonacci problem from codewars in JavaScript

--- codeWars/JavaScript/tribonacci.js
+++ codeWars/JavaScript/tribonacci.js
@@ -2,18 +2,31 @@ function tribonacci(signature,n){
   //your code here
   
   let i = 0;
+  let total = 0;
 
   if(n != 0){
-    while(i < n - 3){
-      let nextValue = signature[i] + signature[i + 1] + signature[i + 2];
-      signature.push(nextValue);
-      i++;
+    if(n > 3){
+      while(i < n - 3){
+        let nextValue = signature[i] + signature[i + 1] + signature[i + 2];
+        signature.push(nextValue);
+        i++;
+      }
+
+      return signature;
+    } else {
+      while(i < n){
+        total += signature[i];
+        i++;
+      }
+
+      return [total]
     }
   } else {
     return [];
   }
 
-  console.log(signature);
+  return signature;
 }
 
 tribonacci([1,2,3],10); // [1,2,3,6,11,20,37,68,125,230]
+tribonacci([7, 0, 0], 1); 
