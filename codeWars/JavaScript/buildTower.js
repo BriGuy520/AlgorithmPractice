7526f5b525f09fca5fdbf5efbf223179ff511b2c
Brian Falasz <bfalasz@yahoo.com>
2022-02-01 22:26:15 -0600

    made some alterations to my solution... mostly made name changes to variables to make the code easier to understand

--- codeWars/JavaScript/buildTower.js
+++ codeWars/JavaScript/buildTower.js
@@ -1,30 +1,63 @@
+// Build Tower
+
+// Build Tower by the following given argument:
+
+// number of floors (integer and always greater than 0).
+
+// Tower block is represented as *
+
+// Python: return a list;
+// JavaScript: returns an Array;
+// C#: returns a string[];
+// PHP: returns an array;
+// C++: returns a vector<string>;
+// Haskell: returns a [String];
+// Ruby: returns an Array;
+// Lua: returns a Table;
+// Have fun!
+
+// for example, a tower of 3 floors looks like below
+
+// [
+//   '  *  ', 
+//   ' *** ', 
+//   '*****'
+// ]
+// and a tower of 6 floors looks like below
+
+// [
+//   '     *     ', 
+//   '    ***    ', 
+//   '   *****   ', 
+//   '  *******  ', 
+//   ' ********* ', 
+//   '***********'
+// ]
+
 function towerBuilder(nFloors) {
   // build here
-  let outerArr = [];
-  let value = "*";
+  let arr = [];
+  let block = "*";
   let base = (nFloors * 2) - 1;
   
   for(let i = 0; i < nFloors; i++){
 
-    let row = (i * 2) + 1
-    let buffer = (base - row) / 2;
-    let rowValue = value.repeat(row).split(" ");
+    let occurancesInRow = (i * 2) + 1
+    let buffer = (base - occurancesInRow) / 2;
+    let rowBlocks = block.repeat(occurancesInRow).split(" ");
 
     for(let j = 0; j < buffer; j++){
-      rowValue.unshift(" ");
+      rowBlocks.unshift(" ");
     }
 
     for(let k = 0; k < buffer; k++){
-      rowValue.push(" ");
+      rowBlocks.push(" ");
     }
 
-    outerArr.push(rowValue.join(""));
+    arr.push(rowBlocks.join(""));
     
   }
 
-  return outerArr;
+  return arr;
   
 }
-
-towerBuilder(1);
-towerBuilder(3);
