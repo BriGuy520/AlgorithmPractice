0674f095ece9742a85c4748f8ce8df76ea08f869
Brian Falasz <bfalasz@yahoo.com>
2022-02-02 20:43:36 -0600

    adding my solution to the format words kata form code wars in JavaScript

--- codeWars/JavaScript/formatWords.js
+++ codeWars/JavaScript/formatWords.js
@@ -1,31 +1,55 @@
+// Complete the method so that it formats the words into a single comma separated value. The last word should be 
+// separated by the word 'and' instead of a comma. The method takes in an array of strings and returns a single formatted string.
+
+// Note:
+
+// Empty string values should be ignored.
+// Empty arrays or null/nil/None values being passed into the method should result in an empty string being returned.
+// Example: (Input --> output)
+
+// ['ninja', 'samurai', 'ronin'] --> "ninja, samurai and ronin"
+// ['ninja', '', 'ronin'] --> "ninja and ronin"
+// [] -->""
+
+
 function formatWords(words){
 
-    console.log(words.join(" ").split(" ").length);
+    let result = words.filter(word => word != "").filter(word => word != ", ");
+    console.log(result);
 
-    switch(words.length){
+    if(words == null) return "";
+
+    switch(result.length){
         case 0:
             return "";
             break;
         case 1:
-            return words[0];
+            return result[0];
             break;
         case 2:
-            return `${words[0]} and ${words[1]}`;
+            return `${result[0]} and ${result[1]}`;
             break;
         default:
-            for(let i = 0; i < words.length - 2; i++){
-                if(words[i] != ""){
-                    words[i] += ", ";
+            for(let i = 0; i < result.length - 2; i++){
+                if(result[i] != ""){
+                    result[i] += ", ";
                 }
             }
     
-            words[words.length - 2] += " and ";
+            result[result.length - 2] += " and ";
+
+            console.log(result.join(""));
+
     
-            return words.join("");
+            return result.join("");
             break;
     }
 }
 
-formatWords(['one', 'two', 'three', '', 'four']);
+formatWords(null);
+
+formatWords(['one','two', '',", "]);
+
+formatWords(['one', 'two', '', 'three', '', 'four']);
 
 
