24ac6be9fcde75f726ef8efe1920067984b9762c
Brian Falasz <bfalasz@yahoo.com>
2022-03-28 20:43:20 -0500

    refactored my solution to find next square to one line

--- codeWars/JavaScript/findNextSquare.js
+++ codeWars/JavaScript/findNextSquare.js
@@ -1,11 +1,8 @@
 function findNextSquare(sq) {
   // Return the next square if sq is a perfect square, -1 otherwise
-  
-  if(Math.sqrt(sq) % 1 === 0){
-    return Math.pow(Math.sqrt(sq) + 1, 2);
-  } else {
-    return -1;
-  }
+
+
+  return Math.sqrt(sq) % 1 === 0 ? Math.pow(Math.sqrt(sq) + 1, 2) : -1;
 
 }
 
