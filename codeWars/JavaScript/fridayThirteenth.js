c386a7710919ddbc50e77aad0e40e539d34d031e
Brian Falasz <bfalasz@yahoo.com>
2022-04-08 21:11:39 -0500

    adding my refactored solution to the friday the thirteenth kata

--- codeWars/JavaScript/fridayThirteenth.js
+++ codeWars/JavaScript/fridayThirteenth.js
@@ -1,13 +1,13 @@
 function fridayTheThirteenths(start, end) {
 
   let arr = [];
-  let months = start === end || !end ? 12 : (end - start + 1) * (12);
+  let months = start === end || !end ? 12 : (end - start + 1) * 12;
 
   for(let i = 0; i < months; i++){
+
     let date = new Date(start, i, 13);
-    if(date.getDay() === 5){
-      arr.push(`${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`);
-    }
+
+    if(date.getDay() === 5) arr.push(`${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`);
   }
 
   return arr.join(" ");
