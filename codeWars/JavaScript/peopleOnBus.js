87c3f7bacbaefbb56cbb455d3c414e712208a75c
Brian Falaz <brianfalasz@gmail.com>
2022-04-22 08:15:33 -0500

    getting started on bus stop kata in code wars with JavaScript

---
+++ codeWars/JavaScript/peopleOnBus.js
@@ -0,0 +1,6 @@
+var number = function(busStops){
+    // Good Luck!
+    
+}
+
+  number([[10,0],[3,5],[5,8]]);
--- codeWars/JavaScript/sumArrays.js
+++ codeWars/JavaScript/sumArrays.js
@@ -2,7 +2,8 @@
 function sum (numbers) {
     "use strict";
       
-    return numbers.reduce((sum, number) => number != null ? sum += number : 0, 0);
+    return numbers.reduce((sum, number) => sum += number, 0);
+
   };
 
   sum([1, 5.2, 4, 0, -1]);
