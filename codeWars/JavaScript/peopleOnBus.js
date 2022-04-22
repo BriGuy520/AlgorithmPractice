0da732edcf04334f77d602eb05cf142a3e7bbdda
Brian Falaz <brianfalasz@gmail.com>
2022-04-22 08:22:14 -0500

    adding my refactored solution to the bus stop kata

--- codeWars/JavaScript/peopleOnBus.js
+++ codeWars/JavaScript/peopleOnBus.js
@@ -1,10 +1,7 @@
 var number = function(busStops){
     // Good Luck!
-    let passengers = 0;
 
-    busStops.forEach(stop => passengers += stop[0] - stop[1]);
-
-    return passengers;
+    return busStops.reduce((passengers, stop) => passengers += stop[0] - stop[1], 0);
 }
 
   number([[10,0],[3,5],[5,8]]);
