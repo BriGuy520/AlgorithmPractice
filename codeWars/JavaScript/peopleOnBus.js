ab491a0557917d8a5bad8a11f8a2b7d55c43c14b
Brian Falaz <brianfalasz@gmail.com>
2022-04-22 08:20:33 -0500

    adding my working solution to the bus stop kata

--- codeWars/JavaScript/peopleOnBus.js
+++ codeWars/JavaScript/peopleOnBus.js
@@ -1,6 +1,10 @@
 var number = function(busStops){
     // Good Luck!
-    
+    let passengers = 0;
+
+    busStops.forEach(stop => passengers += stop[0] - stop[1]);
+
+    return passengers;
 }
 
   number([[10,0],[3,5],[5,8]]);
--- codeWars/JavaScript/sumArrays.js
+++ codeWars/JavaScript/sumArrays.js
@@ -4,7 +4,7 @@ function sum (numbers) {
       
     return numbers.reduce((sum, number) => sum += number, 0);
 
-  };
+};
 
   sum([1, 5.2, 4, 0, -1]);
   sum([]);
