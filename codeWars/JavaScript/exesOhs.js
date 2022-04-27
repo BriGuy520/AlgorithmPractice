6f432d109f2a491c2b11a87bd4c4cc09e6916325
Brian Falaz <brianfalasz@gmail.com>
2022-04-27 08:30:00 -0500

    continuing to make progress on XO kata

--- codeWars/JavaScript/exesOhs.js
+++ codeWars/JavaScript/exesOhs.js
@@ -2,10 +2,17 @@ function XO(str){
     let regexO = /o/g;
     let regexX = /x/g;
 
-    if(regexO.length === regexX.length){
-        return true;
-    } else {
+    let xS = str.match(regexX);
+    let oS = str.match(regexO);
+
+    if(xS.length === 0 || oS.length === 0){
         return false;
+    } else {
+        if(str.match(regexO).length === str.match(regexX).length){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
