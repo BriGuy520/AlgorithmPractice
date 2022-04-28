99e9630515ce58a5c3f296095daa8f4e7c5433df
Brian Falaz <brianfalasz@gmail.com>
2022-04-28 08:34:28 -0500

    removed the console logs from the solution

--- codeWars/JavaScript/exesOhs.js
+++ codeWars/JavaScript/exesOhs.js
@@ -5,24 +5,17 @@ function XO(str){
     let xS = str.toLowerCase().match(regexX);
     let oS = str.toLowerCase().match(regexO);
 
-    console.log(str);
-    console.log(xS);
-    console.log(oS);
 
     if(xS === null || oS === null){
         if(xS === null && oS === null){
-            console.log("true");
             return true;
         } else {
-            console.log("false");
             return false;
         }
     } else {
         if(oS.length === xS.length){
-            console.log("true");
             return true;
         } else {
-            console.log("false");
             return false;
         }
     }
