1524b8b3e607fe87ed8cf874d9e023c4a5802c2b
Brian Falaz <brianfalasz@gmail.com>
2022-04-28 08:33:33 -0500

    working solution to the x and o kata from code wars. All tests pass

--- codeWars/JavaScript/exesOhs.js
+++ codeWars/JavaScript/exesOhs.js
@@ -2,15 +2,27 @@ function XO(str){
     let regexO = /o/g;
     let regexX = /x/g;
 
-    let xS = str.match(regexX);
-    let oS = str.match(regexO);
+    let xS = str.toLowerCase().match(regexX);
+    let oS = str.toLowerCase().match(regexO);
 
-    if(xS.length === 0 || oS.length === 0){
-        return false;
-    } else {
-        if(str.match(regexO).length === str.match(regexX).length){
+    console.log(str);
+    console.log(xS);
+    console.log(oS);
+
+    if(xS === null || oS === null){
+        if(xS === null && oS === null){
+            console.log("true");
             return true;
         } else {
+            console.log("false");
+            return false;
+        }
+    } else {
+        if(oS.length === xS.length){
+            console.log("true");
+            return true;
+        } else {
+            console.log("false");
             return false;
         }
     }
@@ -21,3 +33,5 @@ XO('xxoo');
 XO('xooxx');
 XO('ooxxo');
 XO('xoxoxo');
+XO('x');
+XO('OX');
