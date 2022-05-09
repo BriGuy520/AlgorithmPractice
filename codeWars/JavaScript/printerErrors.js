5c24575b8415b3c61ae55befd1051e1bb24c2945
Brian Falaz <brianfalasz@gmail.com>
2022-05-09 08:40:20 -0500

    had a working solution which I have commented out. Want to refactor and solve another way before submitting

--- codeWars/JavaScript/printerErrors.js
+++ codeWars/JavaScript/printerErrors.js
@@ -1,12 +1,16 @@
 function printerError(s) {
     // your code
-    let errors = 0;
 
-    for(let i = 0; i < s.length; i++){
-        if(s.charCodeAt(i) > 109){
-            errors++;
-        }
-    }
 
-    return `${errors}/${s.length}`;
+    let regex = /[n-z]/g;
+
+    // for(let i = 0; i < s.length; i++){
+    //     if(s.charCodeAt(i) > 109){
+    //         errors++;
+    //     }
+    // }
+
+    return s.match(regex).length != null ? `${s.match(regex).length}/${s.length}` : `0/${s.length}`;
 }
+
+printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz");
