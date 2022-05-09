bacfdb8c2662d08d1ec15f70c013d7bd836b3d9e
Brian Falaz <brianfalasz@gmail.com>
2022-05-09 08:48:10 -0500

    changed my original answer from a for-loop to a regex. Removed the length check on the conditional to make it pass

--- codeWars/JavaScript/printerErrors.js
+++ codeWars/JavaScript/printerErrors.js
@@ -10,7 +10,24 @@ function printerError(s) {
     //     }
     // }
 
-    return s.match(regex).length != null ? `${s.match(regex).length}/${s.length}` : `0/${s.length}`;
+    console.log(s.match(regex) != null ? `${s.match(regex).length}/${s.length}` : `0/${s.length}`);
+
+    return s.match(regex) != null ? `${s.match(regex).length}/${s.length}` : `0/${s.length}`;
 }
 
+printerError("aaabbbbhaijjjm");
 printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz");
+
+function printerError(s) {
+    // your code
+
+
+    let errors = 0;
+
+    // for(let i = 0; i < s.length; i++){
+    //     if(s.charCodeAt(i) > 109){
+    //         errors++;
+    //     }
+    // }
+
+
