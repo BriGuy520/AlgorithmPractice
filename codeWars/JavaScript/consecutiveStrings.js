095d308cf4f69865b3c684a0152f2f528f108e20
Brian Falaz <brianfalasz@gmail.com>
2022-05-16 08:30:04 -0500

    adding my solution to the whats in between kata from code wars in JavaScript

--- codeWars/JavaScript/consecutiveStrings.js
+++ codeWars/JavaScript/consecutiveStrings.js
@@ -4,6 +4,10 @@ function longestConsec(strarr, k) {
     if(strarr.length > k || strarr.length === 0 || k <= 0){
         return "";
     } else {
-        
+        for(let i = 0; i < strarr.length; i += k){
+            while(k > 0){
+                     
+            }
+        }
     }
 }
--- codeWars/JavaScript/printerErrors.js
+++ codeWars/JavaScript/printerErrors.js
@@ -1,16 +1,7 @@
 function printerError(s) {
     // your code
 
-
     let regex = /[n-z]/g;
-
-    // for(let i = 0; i < s.length; i++){
-    //     if(s.charCodeAt(i) > 109){
-    //         errors++;
-    //     }
-    // }
-
-    console.log(s.match(regex) != null ? `${s.match(regex).length}/${s.length}` : `0/${s.length}`);
 
     return s.match(regex) != null ? `${s.match(regex).length}/${s.length}` : `0/${s.length}`;
 }
@@ -18,16 +9,18 @@ function printerError(s) {
 printerError("aaabbbbhaijjjm");
 printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz");
 
-function printerError(s) {
-    // your code
+// function printerError(s) {
+//     // your code
 
 
-    let errors = 0;
+//     let errors = 0;
 
-    // for(let i = 0; i < s.length; i++){
-    //     if(s.charCodeAt(i) > 109){
-    //         errors++;
-    //     }
-    // }
+//     for(let i = 0; i < s.length; i++){
+//         if(s.charCodeAt(i) > 109){
+//             errors++;
+//         }
+//     }
 
+//     return `${errors}/${s.length}`;
+// }
 
