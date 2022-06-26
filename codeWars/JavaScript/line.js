21260320ed39e007b40cdde42bb482fca4ae9bfa
Brian Falasz <bfalasz@yahoo.com>
2022-06-25 19:51:00 -0500

    making progress on the supermarket queue problem. Passing more tests

--- codeWars/JavaScript/line.js
+++ codeWars/JavaScript/line.js
@@ -1,27 +1,36 @@
 function queueTime(customers, n) {
+  
+  console.log("Customers: " + customers + " n: " + n);
   //TODO
   let totalTime = 0;
   let checkout = customers.slice(0, n);
-  let line = customers(n, customers.length);
-  let j = 0;
+  let line = customers.slice(n, customers.length);
+  let i = 0;
+
   
   if(n >= customers.length){
     return Math.max(...customers);
   }
+
+  if(customers.length === 0){
+    return 0;
+  }
     
-  for(let i = 0; i < checkout.length; i++){
-    
-    if(checkout[i] > checkout[i + 1]){
-      checkout[i] = checkout[i] - checkout[i + 1];
-      checkout[i + 1] = line[j++];
-    } else {
-      checkout[i + 1] = checkout[i + 1] - checkout[i];
-      checkout[i] = line[j++];
+  while(i < line.length){
+    console.log(checkout);
+    let shortest = Math.min(...checkout);
+     
+    totalTime += shortest;
+    for(let j = 0; j < checkout.length; j++){
+      checkout[j] -= shortest;
+      
+      if(checkout[j] === 0){
+        checkout[j] = line[i++];
+      }    
     }
-    
-    line.shift();
   }
 
-  return totalTime;
-  
+  return totalTime += Math.max(...checkout);
 }
+
+queueTime([28,14,40,14,39,42,49,22,47,39,38,28,26,15,30,27,11,47,32,41], 3);
