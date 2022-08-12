8414b1ca7003bba37ae87f8b482e904d6d9ece34
Brian Falasz <bfalasz@yahoo.com>
2022-08-11 21:32:49 -0500

    adding my solution to the jumping kata from codewars

--- codeWars/JavaScript/strongNumber.js
+++ codeWars/JavaScript/strongNumber.js
@@ -1,14 +1,12 @@
 function strong(n) {
   
-  let total = 1;
+  let total = 0;
   let factorials = 1;
   let numStr = n + "";
   
   for(let i = 0; i < numStr.length; i++){
-
-    for(let j = +numStr[i]; j > 0; j--){
+   for(let j = +numStr[i]; j > 0; j--){
       factorials *= j;
-      console.log(factorials);
     }
 
     total += factorials;
