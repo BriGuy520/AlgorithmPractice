7a17e6f383fa4310cd1369b5893fe8d0e3a0fcb0
Brian Falasz <bfalasz@yahoo.com>
2022-08-14 21:42:17 -0500

    adding slightly refactored answer for tidy numbers kata from codewars

--- codeWars/JavaScript/tidyNumber.js
+++ codeWars/JavaScript/tidyNumber.js
@@ -1,12 +1,9 @@
 function tidyNumber(n){
   //your code here
   let numStr = n +"";
-  
-  for(let i = 0; i < numStr.length; i++){
-    if(numStr[i] > numStr[i + 1]){
-      return false;
-    }
-  }
-  
+
+  for(let i = 0; i < numStr.length; i++)
+    if(numStr[i] > numStr[i + 1]) return false;
+
   return true;
 }
