e353e949d6345fe1c5d2314521389bc8b0ca1373
Brian Falasz <bfalasz@yahoo.com>
2022-08-15 22:34:53 -0500

    adding my second refactored solution to the disarium kata from codewars

--- codeWars/JavaScript/isDisarium.js
+++ codeWars/JavaScript/isDisarium.js
@@ -1,13 +1,7 @@
 function disariumNumber(n){
   //your code here
   let numStr = n + ""
-  let isDisarium = 0;
-  
-  for(let i = 0; i < numStr.length; i++){
-    isDisarium += Math.pow(+numStr[i], (i + 1));
-  }
-  
-  console.log(isDisarium + " " + n);
+  let isDisarium = numStr.split("").reduce((a, v, i) => a += Math.pow(+v, i + 1), 0);
   
   return isDisarium === n ? "Disarium !!" : "Not !!";
 }
