59d4f32727e2a8cb2cb4cbcc69305d3b475a85b1
Brian Falasz <bfalasz@yahoo.com>
2022-10-02 12:05:44 -0500

    adding refactored solution to the title case kata from code wars

--- codeWars/JavaScript/titleCase.js
+++ codeWars/JavaScript/titleCase.js
@@ -1,23 +1,45 @@
+// function titleCase(title, minorWords) {
+  
+//   if(title.length == 0) return '';
+  
+//   let arr = [];
+//   let titleArr = title.toLowerCase().split(" ");
+//   let minorWordsArr = minorWords.split(" ");
+  
+//   console.log(minorWordsArr);
+//   console.log(titleArr);
+
+//   for(let i = 0; i < titleArr.length; i++){
+//     if(minorWordsArr.indexOf(titleArr[i]) == -1 || i == 0){
+//       let newStr = titleArr[i].substring(0, 1).toUpperCase().concat(titleArr[i].substring(1, titleArr[i].length));
+//       arr.push(newStr);
+//     } else {
+//       arr.push(titleArr[i]);
+//     }
+//   }
+
+//   console.log(arr); 
+
+//   return arr.join(" ");
+// }
+
 function titleCase(title, minorWords = "") {
   
-  console.log(minorWords);
-  
   if(title.length == 0) return '';
-  
-  let arr = [];
-  let titleArr = title.toLowerCase().split(" ");
+
   let minorWordsArr = minorWords.toLowerCase().split(" ");
 
-  for(let i = 0; i < titleArr.length; i++){
-    if(minorWordsArr.indexOf(titleArr[i]) == -1 || i == 0){
-      let newStr = titleArr[i].substring(0, 1).toUpperCase().concat(titleArr[i].substring(1, titleArr[i].length));
-      arr.push(newStr);
-    } else {
-      arr.push(titleArr[i]);
-    }
-  }
 
-  return arr.join(" ");
+  return title.toLowerCase().split(" ").reduce((a, v, i) => {
+    if(minorWordsArr.indexOf(v) == -1 || i == 0){
+      let newStr = v.substring(0, 1).toUpperCase().concat(v.substring(1, v.length));
+      a.push(newStr);
+    } else {
+      a.push(v);
+    } 
+
+    return a;
+  }, []).join(" ");
 }
 
 titleCase('a clash of KINGS', 'a an the of'); // A Clash of Kings
