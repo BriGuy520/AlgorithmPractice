0c62276d836419a2794556fc729751125896d270
Brian Falasz <bfalasz@yahoo.com>
2022-12-17 16:27:16 -0600

    refactored my answer to a kata I solved earlier

--- codeWars/TypeScript/which.ts
+++ codeWars/TypeScript/which.ts
@@ -1,21 +1,6 @@
-function inArray(a1: string[], a2: string[]): string[] {
+function inArray(a1: string[], a2: string[]): string[]{
 
-    let answer = [];
-
-    for(let i: number = 0; i < a1.length; i++){
-
-        let regex = new RegExp(`${a1[i]}`, 'g');
-
-        for(let j: number = 0; j < a2.length; j++){
-
-            if(a2[j].match(regex)){
-               answer.indexOf(a1[i]) === -1 ? answer.push(a1[i]) : '';
-            }
-        }
-
-    }
-
-    return answer.sort();
+    return Array.from(new Set(a1)).filter(val => a2.join(" ").includes(val));
 }
 
 inArray(['arp', 'live', 'strong'], ['lively', 'alive', 'harp', 'sharp', 'armstrong']);
