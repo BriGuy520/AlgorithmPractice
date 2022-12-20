d5f67d8eeac1193e4bf2312673b9fb46b18421e8
Brian Falasz <bfalasz@yahoo.com>
2022-12-19 22:45:42 -0600

    passing answer to stock list kata

--- codeWars/TypeScript/stockList.ts
+++ codeWars/TypeScript/stockList.ts
@@ -3,6 +3,7 @@ const stockList = (listOfArt:string[], listOfCat:string[]):string => {
   
   let answer: string = "";
   let total: number|string = 0;
+  let stock: boolean = false;
   
   
   for(let i: number = 0; i < listOfCat.length; i++){
@@ -11,12 +12,12 @@ const stockList = (listOfArt:string[], listOfCat:string[]):string => {
       total += listOfArt[j].indexOf(listOfCat[i]) === 0 ? parseInt(listOfArt[j].match(/\d/g)!.join("")) : 0;
     }
   
-    answer += i + 1 === listOfCat.length ? `(${listOfCat[i]}: ${total})` : `(${listOfCat[i]}: ${total}) - `;
-    
+    answer +=  i + 1 === listOfCat.length ? `(${listOfCat[i]} : ${total})` : `(${listOfCat[i]} : ${total}) - `;
+    if(total > 0) stock = true;
     total = 0;
   }
   
-  return answer;
+  return stock ? answer : '';
 }
 
 const b: string[] = ["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"];
