6e47c07c3e4d87a485c720a2a51b2529c3498e7b
Brian Falasz <bfalasz@yahoo.com>
2022-12-19 22:39:26 -0600

    added a typescript specific feature in my code to fix the null error I was seeing

--- codeWars/TypeScript/stockList.ts
+++ codeWars/TypeScript/stockList.ts
@@ -8,15 +8,13 @@ const stockList = (listOfArt:string[], listOfCat:string[]):string => {
   for(let i: number = 0; i < listOfCat.length; i++){
     for(let j: number = 0; j < listOfArt.length; j++){
 
-      total += listOfArt[j].indexOf(listOfCat[i]) === 0 ? parseInt(listOfArt[j]?.match(/\d/g)?.join("")) : 0;
+      total += listOfArt[j].indexOf(listOfCat[i]) === 0 ? parseInt(listOfArt[j].match(/\d/g)!.join("")) : 0;
     }
   
     answer += i + 1 === listOfCat.length ? `(${listOfCat[i]}: ${total})` : `(${listOfCat[i]}: ${total}) - `;
     
     total = 0;
   }
-
-  console.log(answer);
   
   return answer;
 }
