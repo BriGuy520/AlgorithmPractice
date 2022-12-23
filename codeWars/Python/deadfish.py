8ef05326c94f0d7e2ecd207369b2e373d170bee1
Brian Falasz <bfalasz@yahoo.com>
2022-12-22 21:54:58 -0600

    refactored my code a little bit

--- codeWars/Python/deadfish.py
+++ codeWars/Python/deadfish.py
@@ -5,13 +5,9 @@ def parse(data):
     answer = []
     
     for x in range(len(data)):
-        if data[x] == 'i':
-            total += 1
-        elif data[x] == 'd':
-            total -= 1
-        elif data[x] ==  's':
-            total *= total
-        elif data[x] == 'o':
-            answer.append(total)
+        if data[x] == 'i': total += 1
+        elif data[x] == 'd': total -= 1
+        elif data[x] ==  's': total *= total
+        elif data[x] == 'o': answer.append(total)
     
     return answer
