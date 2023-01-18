ab191e2150faabe5058f6f5e1933c2e763144111
Brian Falasz <bfalasz@yahoo.com>
2023-01-17 22:26:24 -0600

    refactored solution

--- codeWars/Python/sum_middle.py
+++ codeWars/Python/sum_middle.py
@@ -1,8 +1,5 @@
-def sum_middle(arr):
+def sum_array(arr):
     
-    if not arr:
-        return 0
-  
-    return sum(sorted(arr)[1:len(arr) - 1])
+    return 0 if not arr else sum(sorted(arr)[1:len(arr) - 1])
 
 sum_middle({6, 2, 1, 8, 10})
