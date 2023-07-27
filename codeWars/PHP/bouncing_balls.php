38a0016513f22d3ef13f6154a1f2405ea671cbdb
Brian Falasz <brianfalasz@gmail.com>
2023-07-26 22:08:36 -0500

    did a small refactor

--- codeWars/PHP/bouncing_balls.php
+++ codeWars/PHP/bouncing_balls.php
@@ -1,26 +1,22 @@
 <?php 
 
 function bouncingBall($h, $bounce, $window) {
-
-    $total = 0;
+    
+    $total = 1;
 
     // your code
     if($h <= $window || $bounce <= 0 || $bounce >= 1 || $h <= 0){
 
         return -1;
-        
+
     } else {
     
-        $total = 1;
                 
-        while($h > $window){
-            
+        while($h > $window){  
             
             $h = $h * $bounce;
             
-            if($h > $window){
-                $total += 2;
-            }
+            $total += $h > $window ? 2 : 0;
                 
         }
         
