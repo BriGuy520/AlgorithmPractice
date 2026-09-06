a6efe0adcdc81ea497c145bbdc001e952997791e
Brian Falasz <brianfalasz@gmail.com>
2026-09-05 22:08:12 -0500

    add solution to boolean to string kata

---
+++ codeWars/TypeScript/booleanToString.ts
@@ -0,0 +1,9 @@
+/**
+ * Implement a function which convert the given boolean value into its string representation.
+ * Note: Only valid inputs will be given.
+ */
+
+export const booleanToString = (b:boolean):string => {
+  // your code here
+  return b.toString();
+};
