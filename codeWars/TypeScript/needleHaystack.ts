afa63658183c0a8bcbfbfc02fafda9a2a92787be
Brian Falasz <brianfalasz@gmail.com>
2026-09-07 20:35:16 -0500

    solution needle in the haystack kata

---
+++ codeWars/TypeScript/needleHaystack.ts
@@ -0,0 +1,14 @@
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+// After your function finds the needle it should return a message (as a string) that says:
+// "found the needle at position " plus the index it found the needle, so:
+
+// Example(Input --> Output)
+
+// ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+
+export function findNeedle(haystack: any[]):string {
+  // Good luck!
+ return `found the needle at position ${haystack.indexOf('needle')}`
+}
